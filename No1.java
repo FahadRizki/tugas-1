e0850a6b1f621a0b1e2907bd673a17795abdb9b5
FahadRizki <114449269+FahadRizki@users.noreply.github.com>
2022-09-27 07:23:55 +0700

    Add files via upload

---
+++ No1.java
@@ -0,0 +1,13 @@
+
+package TugasSem3;
+
+
+public class No1 {
+    public static void main(String [] args){
+     System.out.println("Belajar java");
+     System.out.println("Sangat mudah sekali");
+     System.out.println("dan sangat menyenangkan");
+    }
+
+    
+}
---
+++ Tugas1.java
@@ -0,0 +1,16 @@
+
+package TugasSem3;
+
+import javax.swing.*;
+
+public class Tugas1 {
+    private static Object JOptionePane;
+    public static void main(String[] args) {
+    String belajar = JOptionPane.showInputDialog("Anda sedang belajar apa");
+    JOptionPane.showMessageDialog(null,"Belajar pemrograman web mudah","Message",JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    }
+
+   
+
